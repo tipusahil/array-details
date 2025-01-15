a834cc0c33cf7a5964a98d3083080ac769a3b173
tipusahil <tipusahil.ctg@gmailcom>
2025-01-15 13:13:18 +0600

    2nd commit of indexOf() methods

---
+++ indexOf.js
@@ -0,0 +1,8 @@
+/* .......indexOf() method er kaj holo array er upodan/kuno data koto number index e ase seta dekar jonno indexOf() method use kora hoi.jmn: */
+
+var fruits = ['lisu', 'am',   'jam',   'katal']; 
+
+console.log(fruits.indexOf('lisu'));//output: 0 >index0 er position e lisu data ta ase.
+console.log(fruits.indexOf('jam'));//output: 0 >index2 er position e lisu data ta ase.
+
+// ekane indexOf() method er madddome array er kon data array er koto number index positon e ase seta deka hoi.
