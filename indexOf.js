87e906f191f03162a01762a5d1ed5dc81ea6bdd4
tipusahil <tipusahil.ctg@gmailcom>
2025-01-15 13:26:41 +0600

    done

--- indexOf.js
+++ indexOf.js
@@ -3,6 +3,9 @@
 var fruits = ['lisu', 'am',   'jam',   'katal']; 
 
 console.log(fruits.indexOf('lisu'));//output: 0 >index0 er position e lisu data ta ase.
-console.log(fruits.indexOf('jam'));//output: 0 >index2 er position e lisu data ta ase.
+console.log(fruits.indexOf('jam'));//output: 2 >index2 er position e lisu data ta ase.
 
 // ekane indexOf() method er madddome array er kon data array er koto number index positon e ase seta deka hoi.
+
+/* jodi array er moddde oi name kuno data/upodan e na thake tkn negative value dekabe.  jmn:  */
+console.log(fruits.indexOf('tomato'))//output: -1 > output e -1/negative value asar karon hocce jodi array er modde nai emn kuno datar index number dekte caile negative man dekabe, negative man dekanur karon hocce false. ar jodi jei data index number dekte  cacci sei data jodi oi array/fruits ee thake taile tkn indexOf() method diye dekte caile positive man dekabe..
---
+++ name.js
@@ -0,0 +1,11 @@
+/*......................nameof_method_functon........................... */
+
+/* xyz er kaj:     */
+
+/* xyz ta kemne kaj kore:   */
+
+
+/* syntex of xyz :   */
+
+
+/* shorthand syntex of xyz:   */
